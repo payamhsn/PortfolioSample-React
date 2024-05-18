71950392cb2fbe504dd7112fe9769e4b0c9edc52
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-18 17:46:45 +0330

    Create Navbar component and style it.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs.js";
 
+import Nav from "./components/Nav";
+
 export default function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
