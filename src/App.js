208e379efcf6ec7a4495b13e29485cc2c2aa85d8
Payam Hoseini <p.hoseini.ch@gmail.com>
2024-05-18 18:48:19 +0330

    Create connection between pages with react router dom.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
-
 //Global Style
-import GlobalStyle from "./components/GlobalStyle";
-
+import GlobalStyle from "./components/GlobalStyle.js";
 //Import Pages
 import AboutUs from "./pages/AboutUs.js";
+import ContactUs from "./pages/ContactUs.js";
+import OurWork from "./pages/OurWork.js";
+import Nav from "./components/Nav.js";
+//Router
+import { Switch, Route } from "react-router-dom";
 
-import Nav from "./components/Nav";
-
-export default function App() {
+function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
+
+export default App;
